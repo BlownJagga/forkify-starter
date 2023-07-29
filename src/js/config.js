2bad64bf8d128e12560b7999dc5d036915d19957
Seyi Oyedeji <Oyedejiseyi98@gmail.com>
2023-07-29 14:22:08 +0100

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7d9929d4-db02-47f8-bcbe-fc865b411adb';
+export const MODAL_CLOSE_SEC = 2.5;
